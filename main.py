a6aaf327f19f0f24d17de8e4ed504395cbaa914a
madvip3r <madviper@gmail.com>
2025-02-17 20:56:51 +0100

    this lesson was fun

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def main():
     num_words = wordCoun(text)
     #print(f"{num_words} words found in document")
     characters = characterCount(text)
-    print(characters)
+    filtered_dict = {key: value for key, value in characters.items() if key.isalpha()}
+    sorted_dict = sorted(filtered_dict.items(), key=lambda item: item[1], reverse=True)
+    for key, value in sorted_dict:
+        print(f"The '{key}' character was found {value} times")
 
 def characterCount(text):
     counter = {}
